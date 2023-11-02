10f302e56bf14e235912f21d9fe58ed424422c67
eduardo-matsunaga <eduardoborgesmatsunaga@gmail.com>
2023-11-02 17:00:43 -0300

    front att

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './pages/home/home'
 import Agendar from './pages/agendar/Agendar'
 import Navbar from "./pages/navbar/navbar"
 import Footer from "./pages/footer/footer"
+import EsqueciSenha from "./pages/esqueciSenha/esqueciSenha"
+import RedefinicaoSenha from "./pages/redefinicaoSenha/redefinicaoSenha"
 
 
 
@@ -24,6 +26,9 @@ function App() {
           <Route path='/Home' element={<Home />}></Route>
           <Route path='/Agendar' element={<Agendar />}></Route>
           <Route path='/footer' element={<Footer />}></Route>
+          <Route path='/esqueciSenha' element={<EsqueciSenha />}></Route>
+          <Route path='/redefinicaoSenha' element={<RedefinicaoSenha />}></Route>
+
       </Routes>
       <Footer/>
    </BrowserRouter>
